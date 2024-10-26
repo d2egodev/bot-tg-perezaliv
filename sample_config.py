2d97b176f2ca0d9d7dc2b3369bc47c9a7eafda2b
d2egodev <159721519+d2egodev@users.noreply.github.com>
2024-10-27 00:10:50 +0300

    zaliv

---
+++ sample_config.py
@@ -0,0 +1,5 @@
+bot_token = "1644868562:AAEfDQuY4PqcDwQtP900lFOdY2L1sal2iwk"
+radio_link = "http://peridot.streamguys.com:7150/Mirchi"
+sudo_chat_id = -1001461928414
+owner_id = 1254831277
+string_name=
